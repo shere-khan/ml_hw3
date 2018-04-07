e805a94f77e63cf18ebfe8880e9cb5ee522cecb9
justin <justin.barry.e@knights.ucf.edu>
2018-04-07 07:01:46 -0400

    used gridsearchcv to find best params for svc. Still at 59%

--- logistic.py
+++ logistic.py
@@ -1,8 +1,9 @@
-import numpy as np
+import numpy as np, math as m
 from sklearn import linear_model
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
-
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score, GridSearchCV, StratifiedKFold
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
@@ -140,13 +141,99 @@ def estimate_poly_fit(X, y):
 
     plt.show()
 
+def svm(X, y):
+    # print("C_range..")
+    # C_range = 10. ** np.arange(-3, 8)
+    # print("gamma_range..")
+    # gamma_range = 10. ** np.arange(-5, 4)
+    # print("dict..")
+    # param_grid = dict(gamma=gamma_range, C=C_range)
+    # print("Grid search..")
+    # grid = GridSearchCV(SVC(), param_grid=param_grid, cv=StratifiedKFold(n_splits=5))
+    # print("Fit..")
+    # grid.fit(X, y)
+    # print("The best classifier is: ", grid.best_estimator_)
+    with open("sklearn_svc_results_k5_nosplit_acc2.txt", "w") as f:
+        # for d in range(1, 5):
+            # for c in range(100, 200):
+        clf = SVC(C=1, gamma=1.00, kernel='poly', degree=3)
+        S = cross_val_score(clf, np.array(X), np.array(y), cv=4)
+        # f.write("d: {0}  c: {1:>2}  error: {2:.4f}\n".format(d, c, S.mean()))
+        # print("d: {0}  c: {1:>2}  error: {2:.4f}".format(d, c, S.mean()))
+        f.write("d: {0}  c: {1:>2}  error: {2:.4f}\n".format(1, 1, S.mean()))
+        print("d: {0}  c: {1:>2}  error: {2:.4f}".format(1, 1, S.mean()))
+        f.write("\n")
+        # print()
+
+def getdata():
+    fn = 'yeast.data'
+    with open(fn) as f:
+        X = []
+        y = []
+        x1 = []
+        for line in f:
+            d = line.split()
+            x1.append(d.pop(0))
+            y.append(d.pop(-1))
+            x = list(map(float, d))
+            for d in x:
+                if d < 0:
+                    print('test1')
+            X.append(x)
+
+    return (x1, X), y
+
+def find_min_max(X):
+    mins = list(map(int, "0 0 0 0 0 0 0 0".split()))
+    maxs = list(map(int, "0 0 0 0 0 0 0 0".split()))
+    for x in X:
+        for i in range(len(x)):
+            mins[i] = min(x[i], mins[i])
+            maxs[i] = max(x[i], maxs[i])
+
+    return mins, maxs
+
+def normalize(X, mins, maxs):
+    scaleddata = []
+    for x in X:
+        ex = []
+        for i in range(len(x)):
+            r = maxs[i] - mins[i]
+            ex.append(min_max_scaling(x[i], mins[i], r))
+        scaleddata.append(ex)
+
+    return scaleddata
+
+def min_max_scaling(d, minn, r):
+    return 2 * ((d - minn) / r) - 1
+
+def set_to_dict(S):
+    D = {}
+    for i, s in enumerate(S):
+
+        D[s] = i + 1
+
+    return D
+
 if __name__ == '__main__':
-    X1 = process_log_reg_x_data()
-    y1 = process_log_reg_y_data()
-    problem1a(X1, y1)
+    # X1 = process_log_reg_x_data()
+    # y1 = process_log_reg_y_data()
+    # problem1a(X1, y1)
 
-    X2 = process_lin_reg_data("q2x.dat")
-    y2 = process_lin_reg_data("q2y.dat")
-    problem1b(X2, y2)
-    estimate_poly_fit(X2, y2)
+    # X2 = process_lin_reg_data("q2x.dat")
+    # y2 = process_lin_reg_data("q2y.dat")
+    # problem1b(X2, y2)
+    # estimate_poly_fit(X2, y2)
 
+    (x1, X), y = getdata()
+    print()
+    mins, maxs = find_min_max(X)
+    X = normalize(X, mins, maxs)
+    D = set_to_dict(set(y))
+    y = list(map(lambda val: D[val], y))
+
+    p = 0.8
+    sx = m.ceil(len(X) * p)
+    sy = m.ceil(len(y) * p)
+    print("svm")
+    svm(np.array(X), np.array(y))
